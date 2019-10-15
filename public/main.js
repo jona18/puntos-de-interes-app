1f57e6714f86f5a475a5b2cd147eeae256da40f1
unknown <yona_18_3@hotmail.com>
2019-10-14 22:48:39 -0300

    Agregada la validacion del formulario.

--- public/main.js
+++ public/main.js
@@ -17,18 +17,27 @@ $(function(){
   form.on('submit', function(e){
     e.preventDefault();
 
-    var formData = form.serialize();
+    var inputNombre = $('#inputNombre').val();
+    var inputDireccion = $('#inputDireccion').val();
+    var inputTelefono = $('#inputTelefono').val();
+    var inputCategoria = $('#inputCategoria').val();
+    var inputCoordenadas = $('#inputCoordenadas').val();
 
-    $.ajax({
-      type: 'POST',
-      url: '/createPuntos',
-      data: formData,
-      datatype: 'json',
-      success: function(data){
-        updateMap(data);
-        abrirPopup(myLayer);
-      }
-    });
+    if(validarCampos(inputNombre, inputDireccion, inputTelefono, inputCategoria, inputCoordenadas)){
+      var formData = form.serialize();
+
+      $.ajax({
+        type: 'POST',
+        url: '/createPuntos',
+        data: formData,
+        datatype: 'json',
+        success: function(data){
+          updateMap(data);
+          abrirPopup(myLayer);
+          console.log(myLayer);
+        }
+      });
+    }
   });
 
   map.on('popupopen', function(e){
@@ -88,5 +97,100 @@ $(function(){
         updateMap(data);
       }
     });
+  }
+
+  function validarNombre(text){
+    var inputNombre = $('#inputNombre');
+    if(text.length > 30 || text.replace(/\s+/g, '').length === 0){
+      inputNombre.focus();
+      inputNombre.popover({animation: true, content: 'Debe llenar este campo correctamente. Maximo de 30 carateres.', trigger: 'focus'});
+      inputNombre.popover('show');
+      setTimeout(function(){
+        inputNombre.popover('hide');
+      }, 2000);
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function validarDireccion(text){
+    var inputDireccion = $('#inputDireccion');
+    if(text.length > 30 || text.replace(/\s+/g, '').length === 0){
+      inputDireccion.focus();
+      inputDireccion.popover({animation: true, content: 'Debe llenar este campo correctamente. Maximo de 30 carateres.', trigger: 'focus'});
+      inputDireccion.popover('show');
+      setTimeout(function(){
+        inputDireccion.popover('hide');
+      }, 2000);
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function validarTelefono(text){
+    var inputTelefono = $('#inputTelefono');
+    if(isNaN(text) || text.replace(/\s+/g, '').length === 0){
+      inputTelefono.focus();
+      inputTelefono.popover({animation: true, content: 'Debe llenar este campo correctamente. Solo numeros y un maximo de 20 carateres.', trigger: 'focus'});
+      inputTelefono.popover('show');
+      setTimeout(function(){
+        inputTelefono.popover('hide');
+      }, 2000);
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function validarCategoria(text){
+    var inputCategoria = $('#inputCategoria');
+    if(inputCategoria.val() !== 'Comercial' && inputCategoria.val() !== 'Residencial' && inputCategoria.val() !== 'Mixta'){
+      inputCategoria.focus();
+      inputCategoria.focus();
+      inputCategoria.popover({animation: true, content: 'Debe seleccionar una opción.', trigger: 'focus'});
+      inputCategoria.popover('show');
+      setTimeout(function(){
+        inputCategoria.popover('hide');
+      }, 2000);
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function validarCoordenadas(text){
+    var inputCoordenadas = $('#inputCoordenadas');
+    if(!/^[-]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$/.test(text)){
+      inputCoordenadas.focus();
+      inputCoordenadas.popover({animation: true, content: 'Debe llenar este campo correctamente.', trigger: 'focus'});
+      inputCoordenadas.popover('show');
+      setTimeout(function(){
+        inputCoordenadas.popover('hide');
+      }, 2000);
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function validarCampos(inputNombre, inputDireccion, inputTelefono, inputCategoria, inputCoordenadas){
+    if(!validarNombre(inputNombre)){
+      return false;
+    }
+    if(!validarDireccion(inputDireccion)){
+      return false;
+    }
+    if(!validarTelefono(inputTelefono)){
+      return false;
+    }
+    if(!validarCategoria(inputCategoria)){
+      return false;
+    }
+    if(!validarCoordenadas(inputCoordenadas)){
+      return false;
+    }
+    return true;
   }
 });
